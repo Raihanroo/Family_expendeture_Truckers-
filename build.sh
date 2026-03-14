a7c2b110aa8b85605879cbf43651f4eed5f45cb8
Raihan <raihanroo21@gmail.com>
2026-03-14 15:27:30 +0600

    Fix pip install externally-managed-environment error

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Build script for Vercel
 
 # Install dependencies
-pip install -r requirements.txt
+pip install --break-system-packages -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
