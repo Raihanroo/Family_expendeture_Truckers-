3f5ed909e25dfe31b51fcf3b9f19827867cd31da
Raihan <raihanroo21@gmail.com>
2026-03-14 14:56:20 +0600

    Remove duplicate API docs from static/admin

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+# API Documentation HTML কে staticfiles এ copy করো
+cp API_Documentation.html staticfiles/API_Documentation.html
